a8d3715935d73a6773f9607a6c2c5e515d3308fc
Ben-Ammar1312 <ben.ammar1312@gmail.com>
2025-10-04 16:17:29 +0100

    init

---
+++ src/test/java/org/example/tas_backend/TasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
